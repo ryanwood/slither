379fc68a1f29f6f9a822f9fb3e7d096afb19f0b6
Ryan Wood <ryan.wood@gmail.com>
2012-01-24 13:53:59 -0500

    Remove spec_helper

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), 'spec_helper')
+require 'slither'
 
 describe Slither::Generator do
   before(:each) do
--- spec/spec_helper.rb
+++
@@ -1,4 +0,0 @@
-require 'rubygems'
-require 'spec'
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'slither'))
- 

b2a9e18dd51524d928e237b1e91aacc72e244c8a
Jorge Garcia <jorge.mgg94@gmail.com>
2023-02-27 21:42:09 -0600

    Test: update generator spec

--- lib/slither/generator.rb
+++ lib/slither/generator.rb
@@ -1,26 +1,46 @@
+# frozen_string_literal: true
+
 module Slither
   class Generator
+    def initialize(definition)
+      @definition = definition
+    end
 
-		def initialize(definition)
-			@definition = definition
-		end
+    def generate(data)
+      @builder = []
 
-		def generate(data)
-	    @builder = []
-	    @definition.sections.each do |section|
-	      content = data[section.name]
-	      if content
-  	      content = [content] unless content.is_a?(Array)
-  	      raise(Slither::RequiredSectionEmptyError, "Required section '#{section.name}' was empty.") if content.empty?
-  	      content.each do |row|
-  	        @builder << section.format(row)
-  	      end
-  	    else
-  	      raise(Slither::RequiredSectionEmptyError, "Required section '#{section.name}' was empty.") unless section.optional
-	      end
-	    end
-	    @builder.join("\n")
-		end
+      sections.each do |section|
+        content = data[section.name]
 
-	end
+        if content
+          content = [content] unless content.is_a?(Array)
+
+          raise_required_section_empty(section) if content.empty?
+
+          content.each do |row|
+            builder << section.format(row)
+          end
+        else
+          raise_required_section_empty(section) unless section.optional
+        end
+      end
+
+      builder.join("\n")
+    end
+
+    private
+
+    attr_reader :definition, :builder
+
+    def sections
+      definition.sections
+    end
+
+    def raise_required_section_empty(section)
+      raise(
+        Slither::RequiredSectionEmptyError,
+        "Required section '#{section.name}' was empty."
+      )
+    end
+  end
 end
--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -1,42 +1,66 @@
-require File.join(File.dirname(__FILE__), 'spec_helper')
-    
-describe Slither::Generator do
-  before(:each) do
-    @definition = Slither.define :test do |d|
+# frozen_string_literal: true
+
+RSpec.describe Slither::Generator do
+  subject { described_class.new(definition) }
+
+  let(:definition) do
+    Slither.define :test do |d|
       d.header do |h|
         h.trap { |line| line[0,4] == 'HEAD' }
         h.column :type, 4
         h.column :file_id, 10
       end
+
       d.body do |b|
         b.trap { |line| line[0,4] =~ /[^(HEAD|FOOT)]/ }
         b.column :first, 10
         b.column :last, 10
       end
+
       d.footer do |f|
         f.trap { |line| line[0,4] == 'FOOT' }
         f.column :type, 4
         f.column :file_id, 10
-      end     
+      end
     end
-    @data = {
-      :header => [ {:type => "HEAD", :file_id => "1" }],
-      :body => [ 
-        {:first => "Paul", :last => "Hewson" },
-        {:first => "Dave", :last => "Evans" }
-      ],
-      :footer => [ {:type => "FOOT", :file_id => "1" }]
-    }   
-    @generator = Slither::Generator.new(@definition)
   end
-  
-  it "should raise an error if there is no data for a required section" do
-    @data.delete :header
-    lambda {  @generator.generate(@data) }.should raise_error(Slither::RequiredSectionEmptyError, "Required section 'header' was empty.")
+
+  describe ".generate" do
+    let(:data) do
+      {
+        :header => [ {:type => "HEAD", :file_id => "1" }],
+        :body => [
+          {:first => "Paul", :last => "Hewson" },
+          {:first => "Dave", :last => "Evans" }
+        ],
+        :footer => [ {:type => "FOOT", :file_id => "1" }]
+      }
+    end
+
+    it "generate a string" do
+      expected = "HEAD         1\n      Paul    Hewson\n      Dave     Evans\nFOOT         1"
+
+      expect(subject.generate(data)).to eq(expected)
+    end
+
+    context "when a required section is not present" do
+      it "raise an error" do
+        data.delete(:header)
+
+        expect do
+          subject.generate(data)
+        end.to raise_error(Slither::RequiredSectionEmptyError)
+      end
+    end
+
+    context "when a required section has no content" do
+      it "raise an error" do
+        data[:header] = []
+
+        expect do
+          subject.generate(data)
+        end.to raise_error(Slither::RequiredSectionEmptyError)
+      end
+    end
   end
-  
-  it "should generate a string" do
-    expected = "HEAD         1\n      Paul    Hewson\n      Dave     Evans\nFOOT         1"
-    @generator.generate(@data).should == expected
-  end 
 end

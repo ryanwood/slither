572606eea3ee580fb3d153e1b8eab897645fcb52
Jorge Garcia <jorge.mgg94@gmail.com>
2023-02-28 22:01:21 -0600

    Refactor: use extract method to make section checks more verbose on definition

--- lib/slither/definition.rb
+++ lib/slither/definition.rb
@@ -9,14 +9,16 @@ module Slither
     end
 
     def section(name, options = {}, &block)
-      raise( ArgumentError, "Reserved or duplicate section name: '#{name}'") if
-        Section::RESERVED_NAMES.include?( name ) ||
-        (@sections.size > 0 && @sections.map{ |s| s.name }.include?( name ))
+      if section_using_reserved_name?(name) || section_already_defined?(name)
+        raise( ArgumentError, "Reserved or duplicate section name: '#{name}'")
+      end
 
       section = Slither::Section.new(name, @options.merge(options))
       section.definition = self
+
       yield(section)
-      @sections << section
+
+      sections << section
       section
     end
 
@@ -28,6 +30,19 @@ module Slither
 
     def method_missing(method, *args, &block)
       section(method, *args, &block)
+    end
+
+    private
+
+    def section_using_reserved_name?(name)
+      Section::RESERVED_NAMES.include?(name)
+    end
+
+    def section_already_defined?(name)
+      return false if sections.empty?
+
+      section_names = sections.map(&:name)
+      section_names.include?(name)
     end
   end
 end

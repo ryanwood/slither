70df3398e5f4c6347409404bd7700c1161b939ee
Jorge Garcia <jorge.mgg94@gmail.com>
2023-03-02 22:02:19 -0600

    Test: update section_spec

--- spec/section_spec.rb
+++ spec/section_spec.rb
@@ -1,112 +1,163 @@
-require File.join(File.dirname(__FILE__), 'spec_helper')
-    
-describe Slither::Section do
-  before(:each) do
-    @section = Slither::Section.new(:body)
-  end
-  
-  it "should have no columns after creation" do
-    @section.columns.should be_empty
-  end
-  
-  it "should know it's reserved names" do
-    Slither::Section::RESERVED_NAMES.should == [:spacer]
-  end
-  
-  describe "when adding columns" do    
-    it "should build an ordered column list" do
-      @section.should have(0).columns
-    
-      col1 = @section.column :id, 10
-      col2 = @section.column :name, 30
-      col3 = @section.column :state, 2
-    
-      @section.should have(3).columns
-      @section.columns[0].should be(col1)
-      @section.columns[1].should be(col2)
-      @section.columns[2].should be(col3)
+# frozen_string_literal: true
+
+RSpec.describe Slither::Section do
+  subject { described_class.new(:body) }
+
+  describe "#initialize" do
+    it "have no columns after creation" do
+      expect(subject.columns).to be_empty
     end
-  
-    it "should create spacer columns" do
-      @section.should have(0).columns
-      @section.spacer(5)
-      @section.should have(1).columns
+
+    it "know its reserved names" do
+      reserved_names = [:spacer]
+
+      reserved_names.each do |name|
+        expect(Slither::Section::RESERVED_NAMES).to include(name)
+      end
     end
-  
-    it "can should override the alignment of the definition" do
-      section = Slither::Section.new('name', :align => :left)
-      section.options[:align].should == :left
-    end
-    
-    it "should use a missing method to create a column" do
-      @section.should have(0).columns
-      @section.first_name 5
-      @section.should have(1).columns
-    end
-    
-    it "should prevent duplicate column names" do
-      @section.column :id, 10
-      lambda { @section.column(:id, 30) }.should raise_error(Slither::DuplicateColumnNameError, "You have already defined a column named 'id'.")
-    end
-    
-    it "should allow duplicate column names that are reserved (i.e. spacer)" do
-      @section.spacer 10
-      lambda { @section.spacer 10 }.should_not raise_error(Slither::DuplicateColumnNameError)
-    end    
-  end
-  
-  it "should accept and store the trap as a block" do
-    @section.trap { |v| v == 4 }
-    trap = @section.instance_variable_get(:@trap)
-    trap.should be_a(Proc)
-    trap.call(4).should == true
-  end
-  
-  describe "when adding a template" do
-    before(:each) do
-      @template = mock('templated section', :columns => [1,2,3], :options => {})
-      @definition = mock("definition", :templates => { :test => @template } )
-      @section.definition = @definition
-    end
-    
-    it "should ensure the template exists" do
-      @definition.stub! :templates => {}
-      lambda { @section.template(:none) }.should raise_error(ArgumentError)
-    end
-    
-    it "should add the template columns to the current column list" do
-      @template.should_receive(:length).and_return(0)
-      @section.template :test
-      @section.should have(3).columns
-    end
-    
-    it "should merge the template option" do
-       @section = Slither::Section.new(:body, :align => :left)
-       @section.definition = @definition
-       @template.should_receive(:length).and_return(0)
-       @template.stub! :options => {:align => :right}
-       @section.template :test
-       @section.options.should == {:align => :left}
+
+    context "when specifying the alignment" do
+      subject { described_class.new(:name, align: alignment) }
+
+      let(:alignment) { :left }
+
+      it "is overriden" do
+        expect(subject.options[:align]).to eq(alignment)
+      end
     end
   end
 
-  describe "when formatting a row" do    
-    before(:each) do
-      @data = { :id => 3, :name => "Ryan" }
+  describe "#column" do
+    it "prevent duplicate column names" do
+      subject.column(:id, 10)
+
+      expect do
+        subject.column(:id, 10)
+      end.to raise_error(Slither::DuplicateColumnNameError)
     end
-    
-    it "should default to string data aligned right" do
-      @section.column(:id, 5)
-      @section.column(:name, 10)      
-      @section.format( @data ).should == "    3      Ryan"      
+
+    it "should allow duplicate column names that are reserved (i.e. spacer)" do
+      subject.spacer(10)
+
+      expect do
+        subject.spacer(10)
+        subject.spacer(5)
+      end.not_to raise_error
     end
-    
-    it "should left align if asked" do
-      @section.column(:id, 5)
-      @section.column(:name, 10, :align => :left)  
-      @section.format(@data).should == "    3Ryan      "      
+
+    context "when building many columns" do
+      it "build an ordered column list" do
+        columns = [
+          subject.column(:id, 10),
+          subject.column(:name, 30),
+          subject.column(:state, 2)
+        ]
+
+        expect(subject.columns).to match(columns)
+      end
     end
-    
+
+    context "when using a method that's not defined" do
+      it "uses method_missing to create a column" do
+        column = subject.first_name(5)
+
+        expect(subject.columns).to match([column])
+      end
+    end
+  end
+
+  describe "#spacer" do
+    it "create spacer columns" do
+      spacer_column = subject.spacer(5)
+
+      expect(subject.columns).to match([spacer_column])
+    end
+  end
+
+  describe "#trap" do
+    context "when trap is a block" do
+      it "accept and store the trap" do
+        subject.trap { |v| v == 4 }
+
+        trap = subject.instance_variable_get(:@trap)
+
+        expect(trap).to be_a(Proc)
+        expect(trap.call(4)).to eq(true)
+      end
+    end
+
+    it "should try to match a line using the trap" do
+      subject.trap { |line| line == "hello" }
+
+      expect(subject.match("hello")).to eq(true)
+      expect(subject.match("goodbye")).to eq(false)
+    end
+  end
+
+  describe "#template" do
+    before do
+      subject.definition = Slither::Definition.new
+    end
+
+    context "when template does not exist" do
+      it "raise an error if the template is not found on the definition" do
+        expect do
+          subject.template(:none).to raise_error(ArgumentError)
+        end
+      end
+    end
+
+    context "when template does exist" do
+      subject { described_class.new(:body, align: subject_alignment) }
+
+      let(:subject_alignment) { :left }
+
+      before do
+        subject.definition.template(:test, align: :right) do |template|
+          template.column(:first, 10)
+        end
+      end
+
+      it "add the template columns to the current column list" do
+        subject.template(:test)
+
+        expect(subject.columns.size).to eq(1)
+      end
+
+      it "merge the template options with the ones on the section" do
+        expect(subject.definition.options[:align]).to eq(:right)
+
+        subject.template(:test)
+
+        expect(subject.definition.options[:align]).to eq(:right)
+        expect(subject.options[:align]).to eq(subject_alignment)
+      end
+    end
+  end
+
+  describe "#format" do
+    let(:data) { { id: 3, name: "Ryan"} }
+
+    it "transform the 'data' hash to the expected format based on the columns width" do
+      subject.column(:id, 5)
+      subject.column(:name, 10)
+
+      data_hash_content_with_15_width = "    3      Ryan"
+
+      expect(subject.format(data)).to eq(data_hash_content_with_15_width)
+    end
+
+    context "when a column is aligned left" do
+      it "format and aligns the expected columns to the left" do
+        subject.column(:id, 5)
+        subject.column(:name, 10, align: :left)
+
+        data_hash_content_with_15_width_left_aligned = "    3Ryan      "
+        expect(subject.format(data)).to eq(data_hash_content_with_15_width_left_aligned)
+      end
+    end
+
+    # TODO: legacy test that has been commented by some reason
     # it "should raise an error if the data and column definitions aren't the same size" do
     #   @section.column(:id, 5)
     #   lambda { @section.format(@data) }.should raise_error(
@@ -115,34 +166,38 @@ describe Slither::Section do
     #   )
     # end
   end
-  
-  describe "when parsing a file" do
-    before(:each) do
-      @line = '   45      Ryan      WoodSC '
-      @section = Slither::Section.new(:body)
-      @column_content = { :id => 5, :first => 10, :last => 10, :state => 2 }      
+
+  describe "#parse" do
+    let(:line) { "   45      Ryan      WoodSC " }
+    let(:columns) do
+      { id: 5, first: 10, last: 10, state: 2 }
     end
-    
-    it "should return a key for key column" do
-      @column_content.each { |k,v| @section.column(k, v) }
-      parsed = @section.parse(@line)
-      @column_content.each_key { |name| parsed.should have_key(name) }
+    let(:expected_result) do
+      { id: "45", first: "Ryan", last: "Wood", state: "SC" }
     end
 
-    it "should not return a key for reserved names" do
-      @column_content.each { |k,v| @section.column(k, v) }
-      @section.spacer 5
-      @section.should have(5).columns
-      parsed = @section.parse(@line)
-      parsed.should have(4).keys
+    before do
+      columns.each do |key, value|
+        subject.column(key, value)
+      end
+    end
+
+    it "parse the line" do
+      expect(subject.parse(line)).to eq(expected_result)
+    end
+
+    context "when section have a column with a reserved name" do
+      before do
+        subject.spacer(5)
+      end
+
+      it "do not return a key for reserved names" do
+        expect(subject.columns.size).to eq(5)
+
+        expect(subject.parse(line)).to eq(expected_result)
+      end
     end
   end
-  
-  it "should try to match a line using the trap" do
-    @section.trap do |line|
-      line == 'hello'
-    end
-    @section.match('hello').should be_true
-    @section.match('goodbye').should be_false
-  end
+
+
 end

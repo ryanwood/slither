fc3319d8d841d0e0d7284031ad5521d530f7f351
Jorge Garcia <jorge.mgg94@gmail.com>
2023-05-28 17:03:50 -0600

    Chore: fix rubocop offenses on definition

--- lib/slither/definition.rb
+++ lib/slither/definition.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Slither
   class Definition
     attr_reader :sections, :templates, :options
@@ -5,18 +7,18 @@ module Slither
     def initialize(options = {})
       @sections = []
       @templates = {}
-      @options = { :align => :right, :by_bytes => true }.merge(options)
+      @options = { align: :right, by_bytes: true }.merge(options)
     end
 
     def section(name, options = {}, &block)
       if section_using_reserved_name?(name) || section_already_defined?(name)
-        raise( ArgumentError, "Reserved or duplicate section name: '#{name}'")
+        raise(ArgumentError, "Reserved or duplicate section name: '#{name}'")
       end
 
       section = Slither::Section.new(name, @options.merge(options))
       section.definition = self
 
-      yield(section)
+      yield(section) if block
 
       sections << section
       section
@@ -24,13 +26,15 @@ module Slither
 
     def template(name, options = {}, &block)
       section = Slither::Section.new(name, @options.merge(options))
-      yield(section)
+      yield(section) if block
       @templates[name] = section
     end
 
+    # rubocop:disable Style/MissingRespondToMissing
     def method_missing(method, *args, &block)
       section(method, *args, &block)
     end
+    # rubocop:enable Style/MissingRespondToMissing
 
     private
 

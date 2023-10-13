bd391ee75955977573c328a2d6f7a3637fa44329
Jorge Garcia <jorge.mgg94@gmail.com>
2023-10-13 16:47:46 -0600

    Chore[rubocop]: Fix offenses in spec/slither/generator_spec

--- spec/slither/generator_spec.rb
+++ spec/slither/generator_spec.rb
@@ -6,19 +6,19 @@ RSpec.describe Slither::Generator do
   let(:definition) do
     Slither.define :test do |d|
       d.header do |h|
-        h.trap { |line| line[0,4] == 'HEAD' }
+        h.trap { |line| line[0, 4] == "HEAD" }
         h.column :type, 4
         h.column :file_id, 10
       end
 
       d.body do |b|
-        b.trap { |line| line[0,4] =~ /[^(HEAD|FOOT)]/ }
+        b.trap { |line| line[0, 4] =~ /[^(HEAD|FOOT)]/ } # rubocop:disable Lint/DuplicateRegexpCharacterClassElement
         b.column :first, 10
         b.column :last, 10
       end
 
       d.footer do |f|
-        f.trap { |line| line[0,4] == 'FOOT' }
+        f.trap { |line| line[0, 4] == "FOOT" }
         f.column :type, 4
         f.column :file_id, 10
       end
@@ -28,12 +28,12 @@ RSpec.describe Slither::Generator do
   describe ".generate" do
     let(:data) do
       {
-        :header => [ {:type => "HEAD", :file_id => "1" }],
-        :body => [
-          {:first => "Paul", :last => "Hewson" },
-          {:first => "Dave", :last => "Evans" }
+        header: [{ type: "HEAD", file_id: "1" }],
+        body: [
+          { first: "Paul", last: "Hewson" },
+          { first: "Dave", last: "Evans" }
         ],
-        :footer => [ {:type => "FOOT", :file_id => "1" }]
+        footer: [{ type: "FOOT", file_id: "1" }]
       }
     end
 
